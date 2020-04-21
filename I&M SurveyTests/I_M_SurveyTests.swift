ddf07d4c2aa40873694fc493e92a14698fdd29a1
root <morris.murega100@gmail.com>
2020-04-21 14:42:41 +0300

    Initial Commit

---
+++ I&M SurveyTests/I_M_SurveyTests.swift
@@ -0,0 +1,34 @@
+//
+//  I_M_SurveyTests.swift
+//  I&M SurveyTests
+//
+//  Created by Eclectics on 21/04/2020.
+//  Copyright © 2020 Eclectics. All rights reserved.
+//
+
+import XCTest
+@testable import I_M_Survey
+
+class I_M_SurveyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
